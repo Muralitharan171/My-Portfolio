a928803ca4e3b832128420e5b063b93fbd8bcf64
Muralitharan171 <murran171@gmail.com>
2023-07-29 21:49:32 +0530

    commit

--- src/Skills/skills.js
+++ src/Skills/skills.js
@@ -105,7 +105,7 @@ function Cloud({ count = 4, radius = 20 }) {
     const phiSpan = Math.PI / (count + 1);
     const thetaSpan = (Math.PI * 2) / count;
     for (let i = 1; i < count + 1; i++)
-      // Taken from https://discourse.threejs.org/t/can-i-place-obects-on-a-sphere-surface-evenly/4773/6
+     
       for (let j = 0; j < count; j++)
         temp.push([
           new THREE.Vector3().setFromSpherical(
