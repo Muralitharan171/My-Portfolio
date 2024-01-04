484d92a01f7e63e6730477fda772aae2116a8592
Muralitharan171 <murran171@gmail.com>
2024-01-05 02:01:43 +0530

    commmmit

--- src/Projectss/Projectss.js
+++ src/Projectss/Projectss.js
@@ -64,6 +64,32 @@ const Projectss = () => {
         </div>
 
         <div className="project-content">
+          <h1>Youtube Clone</h1>
+          <img src={youtubeImage} alt="project-name" />
+          <div className="btn-group">
+            <a
+              href={"https://github.com/Muralitharan171/Youtube-clone"}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn-repo"
+            >
+              Front-End Source code
+            </a>
+            <a
+              href={"https://warm-cheesecake-17404b.netlify.app/"}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="btn-site"
+            >
+              Live Site
+            </a>
+            
+          </div>
+        </div>
+
+
+
+        <div className="project-content">
           <h1>Portfolio</h1>
           <img src={portfolioImage} alt="project-name" />
           <div className="btn-group">
